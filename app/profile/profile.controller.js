69e25d2049050a087076be6d9217cb35dda01c55
kylemcco <kyle.p.mccormick@gmail.com>
2015-06-02 20:29:30 -0500

    added user stories to /profile

--- app/profile/profile.controller.js
+++ app/profile/profile.controller.js
@@ -2,24 +2,16 @@ angular
   .module('exquisite')
   .controller('ProfileCtrl', ProfileCtrl);
 
-function ProfileCtrl($routeParams, profileFactory, BASE_URL) {
+function ProfileCtrl($routeParams, $firebaseArray, profileFactory, BASE_URL) {
   var vm  = this,
       fb  = new Firebase(BASE_URL),
       id  = fb.getAuth().uid;
 
+  vm.canvases = $firebaseArray(fb.child('/canvas'));
+  vm.userId = id;
+
   profileFactory.findOne(id, function (user) {
     vm.user = user;
   });
-
-  vm.updateProfile = function(){
-    var profileData = {
-      name: vm.user.name,
-    };
-    fb.child('users/' + id + "/profile").update(profileData);
-  };
-
-  vm.sidebarToggle = function() {
-    $("#wrapper").toggleClass("toggled");
-  };
 
 }
--- app/profile/profile.factory.js
+++ app/profile/profile.factory.js
@@ -2,16 +2,16 @@ angular
   .module('exquisite')
   .factory('profileFactory', profileFactory);
 
-function profileFactory($http, BASE_URL) {
+function profileFactory($http, $stateParams, BASE_URL) {
   var profile = {};
 
-  profile.findOne = function (user, cb) {
+  profile.findOne = function (id, cb) {
     $http
-      .get(BASE_URL + 'users/' + user + '/profile.json')
+      .get(BASE_URL + 'users/' + id + '/profile.json')
       .success(function (data) {
         cb(data);
       });
   };
-
+  
   return profile;
 }
