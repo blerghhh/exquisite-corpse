09b33396454166f4456fdbfe070e89f783fcad9e
kylemcco <kyle.p.mccormick@gmail.com>
2015-06-07 16:35:00 -0500

    create link to randomly selected public story

--- app/nav/nav.controller.js
+++ app/nav/nav.controller.js
@@ -2,14 +2,13 @@ angular
   .module('exquisite')
   .controller('NavCtrl', NavCtrl);
 
-function NavCtrl ($location, $http, BASE_URL) {
+function NavCtrl ($location, $http, $firebaseArray, BASE_URL) {
   var vm = this,
       fb = new Firebase(BASE_URL),
       id;
 
   if (fb.getAuth()) {
     id = fb.getAuth().uid;
-
     $http
       .get(BASE_URL + 'users/' + id + '/profile.json')
       .success(function (data) {
@@ -24,4 +23,20 @@ function NavCtrl ($location, $http, BASE_URL) {
   vm.sidebarToggle = function() {
     $(".wrapper").toggleClass("toggled");
   };
+
+  vm.randomCanvas = function() {
+    var canvases = $firebaseArray(fb.child('/canvas')),
+        arr      = [],
+        randomCanvas;
+    canvases.$loaded().then(function(canvases){
+      canvases.forEach(function(i){
+        if (i.status.private === false) {
+          arr.push(i.$id);
+        }
+      });
+      randomCanvas = arr[Math.floor(Math.random() * arr.length)];
+      $location.path('/canvas/' + randomCanvas);
+    });
+  };
+
 }
