6bc67f3b841ef6968b8c34334db9403c85e78538
kylemcco <kyle.p.mccormick@gmail.com>
2015-06-08 16:53:20 -0500

    storyController refactor

--- app/story/story.controller.js
+++ app/story/story.controller.js
@@ -3,47 +3,41 @@ angular
   .controller('StoryCtrl', StoryCtrl);
 
   function StoryCtrl ($stateParams, $scope, $location, $firebaseArray, $firebaseObject, storyFactory, BASE_URL) {
-    var vm               = this,
-        fb               = new Firebase(BASE_URL),
-        id               = $stateParams.uuid,
-        user             = fb.getAuth().uid,
-        fbUser           = fb.child('/users/'),
-        fbStory         = fb.child('/story/' + id),
+    var vm         = this,
+        fb         = new Firebase(BASE_URL),
+        id         = $stateParams.uuid,
+        user       = fb.getAuth().uid,
+        fbUser     = fb.child('/users/'),
+        fbStory    = fb.child('/story/' + id),
         counter,
         wordCount,
         userName;
 
-    vm.messages      = $firebaseArray(fbStory.child('/messages'));
-    vm.stories      = $firebaseArray(fb.child('/story'));
-    vm.story        = $firebaseObject(fbStory);
-    vm.storyInfo    = $firebaseObject(fbStory.child('/info'));
-    vm.messageCount  = 0;
-    vm.user          = fb.getAuth().uid;
+    vm.story       = $firebaseObject(fbStory);
+    vm.storyInfo   = $firebaseObject(fbStory.child('/info'));
+    vm.user        = fb.getAuth().uid;
+    vm.currentUser = $firebaseObject(fb.child('/users/' + user));
 
     vm.story.$bindTo($scope, "data");
-
-    storyFactory.findOne(id, function (story) {
-      vm.info = story.info;
-      counter = vm.info.counter;
-      wordCount = vm.info.wordCount;
+    vm.story.$loaded().then(function(data){
+      if (data.messages != undefined) {
+        vm.messageCount = data.messages.length
+      }
     });
 
     storyFactory.findUser(user, function (userInfo) {
       vm.username = userInfo.profile.username;
     });
 
-    vm.messages.$loaded().then(function(messages){
-      vm.messageCount = messages.length;
-      return vm.messageCount;
-    });
+
 
     ///////// Functions ///////////
 
     vm.addMessage = function() {
-      if (vm.info.counter < vm.info.format.length - 1) {
-        vm.post = {text: vm.newMessageText, user: vm.user, username: vm.username};
+      if (vm.story.info.counter < vm.story.info.format.length - 1) {
+        vm.post = {text: vm.newMessageText, user: vm.currentUser.$id, username: vm.currentUser.profile.username};
       } else {
-        vm.post = {text: vm.newMessageText + '.', user: vm.user, username: vm.username};
+        vm.post = {text: vm.newMessageText + '.', user: vm.currentUser.$id, username: vm.currentUser.profile.username};
       }
 
       storyFactory.addMessage(vm.post).then(function(){
@@ -53,13 +47,12 @@ angular
       vm.storyInfo.$loaded().then(function(data){
         if (data.counter < data.format.length - 1) {
           data.counter = data.counter + 1;
-          vm.info.counter = vm.info.counter + 1;
+          vm.story.info.counter = vm.story.info.counter + 1;
         } else {
           data.counter = 0;
-          vm.info.counter = 0;
+          vm.story.info.counter = 0;
         }
         data.wordCount = data.wordCount + 1;
-        data.lastUser = user;
         data.$save();
       });
 
@@ -67,7 +60,7 @@ angular
 
     vm.createStory = function() {
       var newStory  = $firebaseArray(fb.child('/story')),
-          format     = vm.storyFormat.split(" "),
+          format    = vm.storyFormat.split(" "),
           storyData = {
             info: { name: vm.storyName,
                     creator: vm.username,
@@ -80,9 +73,7 @@ angular
           };
 
       newStory.$add(storyData).then(function(story) {
-        console.log(story.key());
         $location.path('/story/' + story.key());
-
       });
 
       vm.storyName = null;
@@ -93,8 +84,8 @@ angular
 
     vm.userNumber = function(author) {
       var arr = [];
-      for (user in vm.messages) {
-        arr.push(vm.messages[user].user);
+      for (user in vm.story.messages) {
+        arr.push(vm.story.messages[user].user);
       }
       users = arr.filter(function(elem, index, self) {
       return index === self.indexOf(elem);
@@ -108,23 +99,6 @@ angular
 
     vm.toggleOff = function() {
       storyFactory.toggleOff();
-    };
-
-    vm.addOrEditStory = function () {
-      storyFactory.create(vm.newStory, function (res) {
-        vm.data[res.name] = vm.newStory;
-        $location.path('/story');
-      });
-    };
-
-    vm.removeStory = function (id) {
-      storyFactory.delete(id, function () {
-        delete vm.data[id];
-      });
-    };
-
-    vm.viewResults = function() {
-      return data.wordsNeeded > data.wordCount;
     };
 
     vm.findUsername = function(id) {
--- app/story/story.factory.js
+++ app/story/story.factory.js
@@ -6,11 +6,11 @@ function storyFactory($http, $firebaseArray, $firebaseObject, $stateParams, BASE
   var fb       = new Firebase(BASE_URL),
       id       = $stateParams.uuid,
       user     = fb.getAuth().uid,
-      fbStory = fb.child('story/' + id),
+      fbStory  = fb.child('story/' + id),
       messages = $firebaseArray(fbStory.child('/messages')),
       info     = $firebaseObject(fbStory.child('/info')),
       status   = $firebaseObject(fbStory.child('/status')),
-      story   = {};
+      story    = {};
 
   story.addMessage = function(data) {
     id = $stateParams.uuid;
@@ -18,20 +18,6 @@ function storyFactory($http, $firebaseArray, $firebaseObject, $stateParams, BASE
     messages = $firebaseArray(fbStory.child('/messages'));
 
     return messages.$add(data);
-  };
-
-  story.update = function(data) {
-
-    vm.storyInfo.$loaded().then(function(data){
-      if (data.counter < data.format.length - 1) {
-        data.counter = data.counter + 1;
-        vm.info.counter = vm.info.counter + 1;
-      } else {
-        data.counter = 0;
-        vm.info.counter = 0;
-      }
-      data.$save();
-    });
   };
 
   story.findOne = function (id, cb) {
